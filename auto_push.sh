37b83a338620e8a1fdb9b7b117c12abcf73ca1c8
Iky Fareza <133788550+Pangleran@users.noreply.github.com>
2025-11-22 12:24:15 +0000

    auto commit Sat Nov 22 12:24:15 PM UTC 2025

--- auto_push.sh
+++ auto_push.sh
@@ -2,12 +2,19 @@
 
 while true
 do
-  # cek apakah ada perubahan
+  # 1. Pull dulu untuk sync
+  git pull --rebase
+
+  # 2. Jika ada perubahan file, commit + push
   if ! git diff-index --quiet HEAD --; then
     git add .
     git commit -m "auto commit $(date)"
     git push
     echo "Auto committed & pushed at $(date)"
+  else
+    # jika tidak ada perubahan, coba push sisa rebase
+    git push
   fi
+  
   sleep 10
 done
