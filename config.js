660a7eacc59469f9b7ac9ba856bf5ad9f0a57c0b
Iky Fareza <133788550+Pangleran@users.noreply.github.com>
2025-11-22 12:21:19 +0000

    auto commit Sat Nov 22 12:21:19 PM UTC 2025

--- config.js
+++ config.js
@@ -2,7 +2,8 @@ module.exports = {
     "token": "token",
     "owoId": "408785106942164992",
     "solvekey": "",
-    ""
+    "userkey": "",
+    "imagekey": "",
     "channelId": null,
     "cmdRun": "wrun",
     "cmdStop": "ws",
